4832f4e9c90a7da918276e73ed3cb502ed939a1d
João Victor Marques de Freitas <joaovictor@unissp.lan.br>
2026-06-22 16:21:37 -0300

    feat: adicionado token_required na renderização de páginas dashhboard e equipamentos

--- routes/render_pages/render_dashboard.py
+++ routes/render_pages/render_dashboard.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from middlewares.jwt_middleware import token_required
 
 bp_render_dashboard = Blueprint("bp_render_dashboard", __name__)
 
 @bp_render_dashboard.route("/dashboard")
+@token_required
 def render_login():
     return render_template("dashboard.html")
--- routes/render_pages/render_equipments.py
+++ routes/render_pages/render_equipments.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template
+from middlewares.jwt_middleware import token_required
 
 bp_render_equipments = Blueprint("bp_render_equipments", __name__)
 
 @bp_render_equipments.route("/equipamentos")
+@token_required
 def render_equipments():
     return render_template("equipments.html")
