497b6271405181f19d93db04aef7dd3e601daa72
João Victor Marques de Freitas <joaovictor@unissp.lan.br>
2026-06-24 15:02:49 -0300

    feat: adicionado link das páginas na navbar

--- routes/render_pages/render_dashboard.py
+++ routes/render_pages/render_dashboard.py
@@ -5,5 +5,5 @@ bp_render_dashboard = Blueprint("bp_render_dashboard", __name__)
 
 @bp_render_dashboard.route("/dashboard")
 @token_required
-def render_login():
+def render_dashboard():
     return render_template("dashboard.html")
