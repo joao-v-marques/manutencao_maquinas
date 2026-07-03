f009bbb75f2750ed9fa4f01b83e7f8801fd4973e
joao-v-marques <joaovmarques2006@outlook.com>
2026-07-03 13:54:21 -0300

    feat: adicionado o maintenance_group e o id do maintenance_group no payload do token

--- utils/jwt_handler.py
+++ utils/jwt_handler.py
@@ -6,6 +6,8 @@ def generated_token(user):
         "id": user.id,
         "username": user.username,
         "role_description": user.role,
+        "maintenance_group_id": user.maintenance_group_id,
+        "maintenance_group": user.maintenance_group,
         "exp": datetime.utcnow() + timedelta(hours=8)
     }
 
